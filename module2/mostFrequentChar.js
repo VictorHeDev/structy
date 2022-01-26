bae13c57d4c482f7850df15a0e4a328ef3884574
Woodstock <victorhe97@gmail.com>
2022-01-26 01:46:51 -0500

    Init most frequent chars and brute force approach

---
+++ module2/mostFrequentChar.js
@@ -0,0 +1,35 @@
+/*
+most frequent char
+
+Write a function, mostFrequentChar, that takes in a string as an argument. The function should return the most frequent character of the string. If there are ties, return the character that appears earlier in the string.
+
+You can assume that the input string is non-empty.
+test_00:
+
+mostFrequentChar('bookeeper'); // -> 'e'
+
+test_01:
+
+mostFrequentChar('david'); // -> 'd'
+*/
+
+// brute force
+const mostFrequentChar = (s) => {
+  let counter = {};
+  let maximum = -Infinity;
+  let returnChar;
+
+  for (let char of s) {
+    if (!(char in counter)) counter[char] = 0;
+    counter[char] += 1;
+  }
+
+  for (let char in counter) {
+    if (counter[char] > maximum) {
+      maximum = counter[char];
+      returnChar = char;
+    }
+  }
+
+  return returnChar;
+};
---
+++ module2/most_frequent_chars.py
@@ -0,0 +1,16 @@
+"""
+most frequent char
+
+Write a function, most_frequent_char, that takes in a string as an argument. The function should return the most frequent character of the string. If there are ties, return the character that appears earlier in the string.
+
+You can assume that the input string is non-empty.
+test_00:
+
+most_frequent_char('bookeeper') # -> 'e'
+
+test_01:
+
+most_frequent_char('david') # -> 'd'
+"""
+
+# brute force
