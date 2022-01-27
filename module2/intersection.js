b20d9342ffbed89f1c9186dacd3e12c3377f6ff1
Woodstock <victorhe97@gmail.com>
2022-01-27 01:41:31 -0500

    Modify solution for intersection.py to include list comprehension

--- module2/intersection.js
+++ module2/intersection.js
@@ -11,6 +11,10 @@ intersection([4,2,1,6], [3,6,9,2,10]) // -> [2,6]
 test_01:
 
 intersection([2,4,6], [4,2]) // -> [2,4]
+
+Approach
+- use a Set for constant lookup time
+- use the Set methods .has() and .add()
 */
 
 // brute force
@@ -23,4 +27,20 @@ const intersection = (a, b) => {
     if (num in store) resultArr.push(num);
   }
   return resultArr;
+};
+
+// solution
+const intersection = (a, b) => {
+  const result = [];
+  const items = new Set();
+
+  for (let item of a) {
+    items.add(item);
+  }
+
+  for (let ele of b) {
+    if (items.has(ele)) result.push(ele);
+  }
+
+  return result;
 };
--- module2/intersection.py
+++ module2/intersection.py
@@ -16,3 +16,8 @@ intersection([2,4,6], [4,2]) # -> [2,4]
 # brute force
 def intersection(a, b):
   return list(set(a).intersection(set(b)))
+
+# solution with list comprehension
+def intersection(a, b):
+  items_set = set(a)
+  return [ ele for ele in b if ele in items_set ]
