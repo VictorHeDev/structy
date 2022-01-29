bf80602d2c9a3ccb14d6ffb6134369c37ef7ceb6
Woodstock <victorhe97@gmail.com>
2022-01-29 00:47:19 -0500

    Solve mergelist js with recursion -- my approach

--- module3/mergeLists.js
+++ module3/mergeLists.js
@@ -106,3 +106,22 @@ const mergeLists = (head1, head2) => {
 };
 
 // recursive
+const mergeLists = (head1, head2) => {
+  if (head1 === null && head2 === null) return null;
+  if (head1 === null) return head2;
+  if (head2 === null) return head1;
+
+  let tail;
+  let next1 = head1;
+  let next2 = head2;
+  if (head1.val < head2.val) {
+    tail = head1;
+    next1 = head1.next;
+  } else {
+    tail = head2;
+    next2 = head2.next;
+  }
+
+  tail.next = mergeLists(next1, next2);
+  return tail;
+};
