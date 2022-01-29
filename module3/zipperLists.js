a3b4ae83b8a1c787efaed2e50499b54768d85af1
Woodstock <victorhe97@gmail.com>
2022-01-28 23:30:03 -0500

    Add recursive solution for zipper list js

--- module3/zipperLists.js
+++ module3/zipperLists.js
@@ -113,3 +113,17 @@ const zipperLists = (head1, head2) => {
 
   return head1;
 };
+
+// recursive solution
+const zipperLists = (head1, head2) => {
+  if (head1 === null && head2 === null) return null;
+  if (head1 === null) return head2;
+  if (head2 === null) return head1;
+
+  const next1 = head1.next;
+  const next2 = head2.next;
+  head1.next = head2;
+  head2.next = zipperLists(next1, next2);
+
+  return head1;
+};
