b022c4084d0f36b371b64449829506a57f159c77
Woodstock <victorhe97@gmail.com>
2022-01-30 19:08:37 -0500

    Init longestStreak js

---
+++ module3/longestStreak.js
@@ -0,0 +1,42 @@
+/*
+longest streak
+
+Write a function, longestStreak, that takes in the head of a linked list as an argument. The function should return the length of the longest consecutive streak of the same value within the list.
+test_00:
+
+const a = new Node(5);
+const b = new Node(5);
+const c = new Node(7);
+const d = new Node(7);
+const e = new Node(7);
+const f = new Node(6);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+// 5 -> 5 -> 7 -> 7 -> 7 -> 6
+
+longestStreak(a); // 3
+
+test_01:
+
+const a = new Node(3);
+const b = new Node(3);
+const c = new Node(3);
+const d = new Node(3);
+const e = new Node(9);
+const f = new Node(9);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+// 3 -> 3 -> 3 -> 3 -> 9 -> 9
+
+longestStreak(a); // 4
+*/
