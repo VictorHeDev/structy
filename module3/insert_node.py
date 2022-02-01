787fd3dbe17569f5c3210f9c927271a1b01884d6
Woodstock <victorhe97@gmail.com>
2022-02-01 16:43:24 -0500

    Init insert node py

---
+++ module3/insert_node.py
@@ -0,0 +1,40 @@
+'''
+insert node
+
+Write a function, insert_node, that takes in the head of a linked list, a value, and an index. The function should insert a new node with the value into the list at the specified index. Consider the head of the linked list as index 0. The function should return the head of the resulting linked list.
+
+Do this in-place.
+
+You may assume that the input list is non-empty and the index is not greater than the length of the input list.
+test_00:
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+insert_node(a, 'x', 2)
+# a -> b -> x -> c -> d
+
+test_01:
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+insert_node(a, 'v', 3)
+# a -> b -> c -> v -> d
+'''
