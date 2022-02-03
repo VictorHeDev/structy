b9a3cb6febc965c8a5720cdc66b88f21ffd1ee0b
Woodstock <victorhe97@gmail.com>
2022-02-02 21:39:05 -0500

    Add recursive solution for addLists js

--- module3/addLists.js
+++ module3/addLists.js
@@ -56,3 +56,21 @@ APPROACH
 4. What if we need to handle a final carry?
 - need one final check for if we hit the ends of the LLs and we sum our carry
 */
+
+const addLists = (head1, head2, carry = 0) => {
+  if (head1 === null && head2 === null && carry === 0) return null;
+  const val1 = head1 === null ? 0 : head1.val;
+  const val2 = head2 === null ? 0 : head2.val;
+
+  const sum = val1 + val2 + carry;
+  const nextCarry = sum > 9 ? 1 : 0;
+  const digit = sum % 10;
+
+  const resultNode = new Node(digit);
+
+  const next1 = head1 === null ? null : head1.next;
+  const next2 = head2 === null ? null : head2.next;
+
+  resultNode.next = addLists(next1, next2, nextCarry);
+  return resultNode;
+};
