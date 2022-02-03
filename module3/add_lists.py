5d14886c9b46120e67e1ac972b5f09eb6789749e
Woodstock <victorhe97@gmail.com>
2022-02-03 00:27:02 -0500

    Add recursive solution for add_lists py

--- module3/add_lists.py
+++ module3/add_lists.py
@@ -41,3 +41,27 @@ b2.next = b3
 add_lists(a1, b1)
 # 5 -> 7 -> 9
 '''
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+# recursive
+def add_lists(head_1, head_2, carry=0):
+  if head_1 is None and head_1 is None and carry == 0:
+    return None
+  val_1 = head_1.val if head_1 is not None else 0
+  val_2 = head_2.val if head_2 is not None else 0
+
+  sum = val_1 + val_2 + carry
+  nextCarry = 1 if sum > 9 else 0
+  digit = sum % 10
+
+  resultNode = Node(digit)
+
+  next_1 = None if head_1 is None else head_1.next
+  next_2 = None if head_2 is None else head_2.next
+  resultNode.next = add_lists(next_1, next_2, nextCarry)
+
+  return resultNode
