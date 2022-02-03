38d47f5dafdbb3d2257aea9164947c83180333b9
Woodstock <victorhe97@gmail.com>
2022-02-03 14:34:16 -0500

    Init DF values

---
+++ module4/depthFirstValues.js
@@ -0,0 +1,58 @@
+/*
+depth first values
+
+Write a function, depthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in depth-first order.
+
+Hey. This is our first binary tree problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
+test_00:
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+depthFirstValues(a);
+//    -> ['a', 'b', 'd', 'e', 'c', 'f']
+
+test_01:
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+const g = new Node('g');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    /
+//   g
+
+depthFirstValues(a);
+//    -> ['a', 'b', 'd', 'e', 'g', 'c', 'f']
+*/
