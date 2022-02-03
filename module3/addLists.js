5ba6fa543228c02f9bd9e79753c9df81efa28c4c
Woodstock <victorhe97@gmail.com>
2022-02-02 23:06:46 -0500

    Add iterative solution for addLists js

--- module3/addLists.js
+++ module3/addLists.js
@@ -57,6 +57,7 @@ APPROACH
 - need one final check for if we hit the ends of the LLs and we sum our carry
 */
 
+// recursive solution
 const addLists = (head1, head2, carry = 0) => {
   if (head1 === null && head2 === null && carry === 0) return null;
   const val1 = head1 === null ? 0 : head1.val;
@@ -73,4 +74,28 @@ const addLists = (head1, head2, carry = 0) => {
 
   resultNode.next = addLists(next1, next2, nextCarry);
   return resultNode;
+};
+
+// iterative solution
+const addLists = (head1, head2) => {
+  const dummyHead = new Node(null);
+  let tail = dummyHead;
+  let carry = 0;
+  let current1 = head1;
+  let current2 = head2;
+
+  while (current1 !== null || current2 !== null || carry === 1) {
+    const val1 = current1 === null ? 0 : current1.val;
+    const val2 = current2 === null ? 0 : current2.val;
+    const sum = val1 + val2 + carry;
+    carry = sum > 9 ? 1 : 0;
+    const digit = sum % 10;
+
+    if (current1 !== null) current1 = current1.next;
+    if (current2 !== null) current2 = current2.next;
+    tail.next = new Node(digit);
+    tail = tail.next;
+  }
+
+  return dummyHead.next;
 };
