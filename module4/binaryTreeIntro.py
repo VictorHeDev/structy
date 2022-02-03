69c959ef08ec7c935a3c0b35bf0928c87630f74f
Woodstock <victorhe97@gmail.com>
2022-02-03 14:33:10 -0500

    Complete intro to BT

--- module4/binaryTreeIntro.py
+++ module4/binaryTreeIntro.py
@@ -4,3 +4,23 @@ class Node:
     self.left = None
     self.right = None
 
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+'''
+          a
+        /   \
+      b       c
+    /   \      \
+  d       e      f
+'''
