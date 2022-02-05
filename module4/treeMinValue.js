5e639e2cb7eaf61cc507e177d2b7d488f27e7b17
Woodstock <victorhe97@gmail.com>
2022-02-05 18:12:39 -0500

    Added DFS iterative to treeMinValue js

--- module4/treeMinValue.js
+++ module4/treeMinValue.js
@@ -86,8 +86,23 @@ const treeMinValue = (root) => {
   return resultMin;
 };
 
-// DFS
+// DFS recursive
 const treeMinValue = (root) => {
   if (root === null) return Infinity;
   return Math.min(root.val, treeMinValue(root.left), treeMinValue(root.right));
+};
+
+// DFS iterative
+const treeMinValue = (root) => {
+  let smallest = Infinity;
+  const stack = [root];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    if (current.val < smallest) smallest = current.val;
+
+    if (current.left !== null) stack.push(current.left);
+    if (current.right !== null) stack.push(current.right);
+  }
+
+  return smallest;
 };
