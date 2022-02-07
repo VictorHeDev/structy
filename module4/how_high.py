894720da1a19f12e5bd526735b94121605443b6a
Woodstock <victorhe97@gmail.com>
2022-02-07 18:48:16 -0500

    Add DFS solution for how high py

--- module4/how_high.py
+++ module4/how_high.py
@@ -56,3 +56,19 @@ e.left = g
 
 how_high(a) # -> 3
 '''
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def how_high(node):
+  if node is None:
+    return -1
+
+  left_tree_height = how_high(node.left)
+  right_tree_height = how_high(node.right)
+
+  return 1 + max(left_tree_height, right_tree_height)
+
