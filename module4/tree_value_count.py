b17debca6bba797a569454d58ae2d9798c00d427
Woodstock <victorhe97@gmail.com>
2022-02-07 18:01:47 -0500

    Add solutions for BFS and DFS tree value count py

--- module4/tree_value_count.py
+++ module4/tree_value_count.py
@@ -48,3 +48,37 @@ c.right = f
 
 tree_value_count(a,  12) # -> 2
 '''
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+# BFS
+from collections import deque
+def tree_value_count(root, target):
+  if root is None:
+    return 0
+  queue = deque([ root ])
+  count = 0
+
+  while queue:
+    current = queue.popleft()
+    if current.val == target:
+      count += 1
+
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+
+  return count
+
+# DFS
+def tree_value_count(root, target):
+  if root is None:
+    return 0
+  match = 1 if root.val == target else 0
+
+  return match + tree_value_count(root.left, target) + tree_value_count(root.right, target)
