a4f9b53cd62e6bf50ab1bd4e6ada99978577dbca
Woodstock <victorhe97@gmail.com>
2022-02-08 13:17:35 -0500

    Add BFS solution for bottom right value py

--- module4/bottom_right_value.py
+++ module4/bottom_right_value.py
@@ -57,3 +57,23 @@ e.right = h
 bottom_right_value(a) # -> 6
 '''
 
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+from collections import deque
+def bottom_right_value(root):
+  queue = deque([ root ])
+  current = None
+
+  while queue:
+    current = queue.popleft()
+
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+
+  return current.val
