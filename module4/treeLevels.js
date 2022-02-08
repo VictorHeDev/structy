1b8736eed3e357b31ebe64b04cee7c26d29065c2
Woodstock <victorhe97@gmail.com>
2022-02-08 15:44:14 -0500

    Init tree paths js

---
+++ module4/treeLevels.js
@@ -0,0 +1,69 @@
+/*
+tree levels
+
+Write a function, treeLevels, that takes in the root of a binary tree. The function should return a 2-Dimensional array where each subarray represents a level of the tree.
+test_00:
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+treeLevels(a); // ->
+// [
+//   ['a'],
+//   ['b', 'c'],
+//   ['d', 'e', 'f']
+// ]
+
+test_01:
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+const g = new Node("g");
+const h = new Node("h");
+const i = new Node("i");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+e.right = h;
+f.left = i;
+
+//         a
+//      /    \
+//     b      c
+//   /  \      \
+//  d    e      f
+//      / \    /
+//     g  h   i
+
+treeLevels(a); // ->
+// [
+//   ['a'],
+//   ['b', 'c'],
+//   ['d', 'e', 'f'],
+//   ['g', 'h', 'i']
+// ]
+*/
