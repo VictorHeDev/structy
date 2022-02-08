db09206752bf9528b1318194dd2547a213072f7a
Woodstock <victorhe97@gmail.com>
2022-02-08 18:09:24 -0500

    Add BFS and DFS for tree levels js

--- module4/treeLevels.js
+++ module4/treeLevels.js
@@ -67,3 +67,86 @@ treeLevels(a); // ->
 //   ['g', 'h', 'i']
 // ]
 */
+
+/*
+APPROACH
+- we take in a binary tree and we want to return a 2D array where every subarray represents a level of the binary tree
+
+- in this example we can use a stack to keep track of the nodes and levels
+- we want to add in both the current node and the level [ node: 3, level: 0 ]
+  - we tack on the level when we do the current.left and current.right operations
+
+*/
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// DFS recursive
+const treeLevels = (root) => {
+  const levels = [];
+  fillLevels(root, levels, 0);
+  return levels;
+};
+
+const fillLevels = (root, levels, levelNum) => {
+  if (root === null) return;
+
+  if (levels.length === levelNum) {
+    levels.push([root.val]);
+  } else {
+    levels[levelNum].push(root.val);
+  }
+
+  fillLevels(root.left, levels, levelNum + 1);
+  fillLevels(root.right, levels, levelNum + 1);
+};
+
+// DFS iterative
+const treeLevels = (root) => {
+  if (root === null) return [];
+
+  let stack = [{ node: root, levelNum: 0 }];
+  let levels = [];
+
+  while (stack.length) {
+    let { node, levelNum } = stack.pop();
+    if (levels.length === levelNum) {
+      levels.push([node.val]);
+    } else {
+      levels[levelNum].push(node.val);
+    }
+
+    if (node.right) stack.push({ node: node.right, levelNum: levelNum + 1 });
+    if (node.left) stack.push({ node: node.left, levelNum: levelNum + 1 });
+  }
+
+  return levels;
+};
+
+// BFS
+const treeLevels = (root) => {
+  if (root === null) return [];
+
+  let queue = [{ node: root, levelNum: 0 }];
+  let levels = [];
+
+  while (queue.length) {
+    let { node, levelNum } = queue.pop();
+
+    if (levels.length === levelNum) {
+      levels.push([node.val]);
+    } else {
+      levels[levelNum].push(node.val);
+    }
+
+    if (node.left) queue.unshift({ node: node.left, levelNum: levelNum + 1 });
+    if (node.right) queue.unshift({ node: node.right, levelNum: levelNum + 1 });
+  }
+
+  return levels;
+};
