283f3bd9278beed87036871369598867f8ae4cc7
Woodstock <victorhe97@gmail.com>
2022-02-08 22:38:31 -0500

    Init tree levels py

---
+++ module4/tree_levels.py
@@ -0,0 +1,70 @@
+'''
+tree levels
+
+Write a function, tree_levels, that takes in the root of a binary tree. The function should return a 2-Dimensional list where each sublist represents a level of the tree.
+test_00:
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+tree_levels(a) # ->
+# [
+#   ['a'],
+#   ['b', 'c'],
+#   ['d', 'e', 'f']
+# ]
+
+test_01:
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+i = Node('i')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+e.right = h
+f.left = i
+
+#         a
+#      /    \
+#     b      c
+#   /  \      \
+#  d    e      f
+#      / \    /
+#     g  h   i
+
+tree_levels(a) # ->
+# [
+#   ['a'],
+#   ['b', 'c'],
+#   ['d', 'e', 'f'],
+#   ['g', 'h', 'i']
+# ]
+'''
+
