d8c8148705d88a9a25298e1eb4a20050e351c530
Woodstock <victorhe97@gmail.com>
2022-02-08 22:38:54 -0500

    Add solutions for BFS and DFS tree levels py

--- module4/tree_levels.py
+++ module4/tree_levels.py
@@ -68,3 +68,74 @@ tree_levels(a) # ->
 # ]
 '''
 
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+# DFS iterative
+def tree_levels(root):
+  if root is None:
+    return []
+
+  levels = []
+  stack = [(root, 0)]
+
+  while stack:
+    node, level_num = stack.pop()
+
+    if len(levels) == level_num:
+      levels.append([ node.val ])
+    else:
+      levels[level_num].append(node.val)
+
+    if node.right:
+      stack.append((node.right, level_num + 1))
+    if node.left:
+      stack.append((node.left, level_num + 1))
+
+  return levels
+
+# DFS recursive
+def tree_levels(root):
+  levels = []
+  _tree_levels(root, levels, 0)
+  return levels
+
+def _tree_levels(root, levels, level_num):
+  if root is None:
+    return;
+
+  if len(levels) == level_num:
+    levels.append([ root.val ])
+  else:
+    levels[level_num].append(root.val)
+
+  _tree_levels(root.left, levels, level_num + 1)
+  _tree_levels(root.right, levels, level_num + 1)
+
+# BFS
+from collections import deque
+
+def tree_levels(root):
+  if root is None:
+    return []
+
+  levels = []
+  queue = deque([ (root, 0) ])
+
+  while queue:
+    node, level_num = queue.popleft()
+
+    if len(levels) == level_num:
+      levels.append([node.val])
+    else:
+      levels[level_num].append(node.val)
+
+    if node.left:
+      queue.append((node.left, level_num + 1))
+    if node.right:
+      queue.append((node.right, level_num + 1))
+
+  return levels
