20dad714d9c1c68053d68bb973ed54a0c1455135
Woodstock <victorhe97@gmail.com>
2022-02-11 17:30:54 -0500

    Init has path py

---
+++ module5/has_path.py
@@ -0,0 +1,45 @@
+'''
+has path
+
+Write a function, has_path, that takes in a dictionary representing the adjacency list of a directed acyclic graph and two nodes (src, dst). The function should return a boolean indicating whether or not there exists a directed path between the source and destination nodes.
+
+Hey. This is our first graph problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
+test_00:
+
+graph = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+has_path(graph, 'f', 'k') # True
+
+test_01:
+
+graph = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+has_path(graph, 'f', 'j') # False
+
+test_02:
+
+graph = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+has_path(graph, 'i', 'h') # True
+'''
