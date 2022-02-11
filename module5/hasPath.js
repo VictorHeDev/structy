3f1d368d21bf801b186ffd444251f6c0822f3507
Woodstock <victorhe97@gmail.com>
2022-02-11 17:30:13 -0500

    Add DFS solution for has path js

--- module5/hasPath.js
+++ module5/hasPath.js
@@ -59,3 +59,14 @@ const hasPath = (graph, src, dst) => {
 
   return false;
 };
+
+// DFS
+const hasPath = (graph, src, dst) => {
+  if (src === dst) return true;
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst) === true) return true;
+  }
+
+  return false;
+};
