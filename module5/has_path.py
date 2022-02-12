195a134cdb0e095f635527cbc487caba553232af
Woodstock <victorhe97@gmail.com>
2022-02-12 10:58:04 -0500

    Add BFS solution for has path py

--- module5/has_path.py
+++ module5/has_path.py
@@ -44,3 +44,18 @@ graph = {
 has_path(graph, 'i', 'h') # True
 '''
 
+# BFS
+from collections import deque
+def has_path(graph, src, dst):
+  queue = deque([ src ])
+
+  while queue:
+    current = queue.popleft()
+    if current == dst:
+      return True
+
+    for neighbor in graph[current]:
+      queue.append(neighbor)
+
+  return False
+
