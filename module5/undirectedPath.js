83620c5eb7095b4daec709da1d8924e73ccbb436
Woodstock <victorhe97@gmail.com>
2022-02-12 16:08:28 -0500

    Init undirected path js

---
+++ module5/undirectedPath.js
@@ -0,0 +1,42 @@
+/*
+undirected path
+
+Write a function, undirectedPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). The function should return a boolean indicating whether or not there exists a path between nodeA and nodeB.
+test_00:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'j', 'm'); // -> true
+
+test_01:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'm', 'j'); // -> true
+
+test_02:
+
+const edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+undirectedPath(edges, 'l', 'j'); // -> true
+
+
+*/
