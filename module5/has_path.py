56a5a7cf6995018ec6041f582b418253f808d3e9
Woodstock <victorhe97@gmail.com>
2022-02-12 10:58:22 -0500

    Add DFS solution for has path py

--- module5/has_path.py
+++ module5/has_path.py
@@ -59,3 +59,13 @@ def has_path(graph, src, dst):
 
   return False
 
+# DFS
+def has_path(graph, src, dst):
+  if src == dst:
+    return True
+
+  for neighbor in graph[src]:
+    if has_path(graph, neighbor, dst) == True:
+      return True
+
+  return False
