0ce694c3bea05ea8746fb7370e5fde77f5bf52a9
Woodstock <victorhe97@gmail.com>
2022-02-12 16:59:20 -0500

    Add solution for undirected path js

--- module5/undirectedPath.js
+++ module5/undirectedPath.js
@@ -37,6 +37,44 @@ const edges = [
 ];
 
 undirectedPath(edges, 'l', 'j'); // -> true
-
-
 */
+
+/*
+APPROACH
+- given the edges of an undirected graph
+- every pair represents a connection between two nodes
+- we want to convert the edges into an adjacency list
+- use a set to check if a node has been visited
+*/
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  return hasPath(graph, nodeA, nodeB, new Set());
+};
+
+const buildGraph = (edges) => {
+  graph = {};
+
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  return graph;
+};
+
+const hasPath = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+
+  visited.add(src);
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) return true;
+  }
+
+  return false;
+};
