9d04a152b006f0f545684b3f9f56a2317cb89fc7
Woodstock <victorhe97@gmail.com>
2022-02-13 19:31:27 -0500

    Add solution for connected components count py

--- module5/connected_components_count.py
+++ module5/connected_components_count.py
@@ -38,3 +38,23 @@ connected_components_count({
   2: [1]
 }) # -> 3
 '''
+
+def connected_components_count(graph):
+  visited = set()
+  count = 0
+
+  for node in graph:
+    if explore(graph, node, visited) == True:
+      count += 1
+
+  return count
+
+def explore(graph, current, visited):
+  if current in visited:
+    return False
+
+  visited.add(current)
+  for neighbor in graph[current]:
+    explore(graph, neighbor, visited)
+
+  return True
