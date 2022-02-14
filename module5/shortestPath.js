49361d80a7e74b7dc4a95435d747545ab8fdbfdc
Woodstock <victorhe97@gmail.com>
2022-02-14 13:15:22 -0500

    Init shortest path js

---
+++ module5/shortestPath.js
@@ -0,0 +1,42 @@
+/*
+shortest path
+
+Write a function, shortestPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1.
+test_00:
+
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+];
+
+shortestPath(edges, 'w', 'z'); // -> 2
+
+test_01:
+
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+];
+
+shortestPath(edges, 'y', 'x'); // -> 1
+
+test_02:
+
+const edges = [
+  ['a', 'c'],
+  ['a', 'b'],
+  ['c', 'b'],
+  ['c', 'd'],
+  ['b', 'd'],
+  ['e', 'd'],
+  ['g', 'f']
+];
+
+shortestPath(edges, 'a', 'e'); // -> 3
+*/
