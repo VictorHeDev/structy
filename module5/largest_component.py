4491bf19a12903ea870de536bd03d55f7e68256e
Woodstock <victorhe97@gmail.com>
2022-02-13 20:54:31 -0500

    Add solution for largest component py

--- module5/largest_component.py
+++ module5/largest_component.py
@@ -39,3 +39,23 @@ largest_component({
 }) # -> 5
 '''
 
+def largest_component(graph):
+  visited = set()
+  largest = 0
+
+  for node in graph:
+    size = explore_size(graph, node, visited)
+    largest = max(size, largest)
+
+  return largest
+
+def explore_size(graph, current, visited):
+  if current in visited:
+    return 0
+  visited.add(current)
+  size = 1
+
+  for neighbor in graph[current]:
+    size += explore_size(graph, neighbor, visited)
+
+  return size
