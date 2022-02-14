66fe17fb3694a20f587063d7f3b1b59a82b1e1de
Woodstock <victorhe97@gmail.com>
2022-02-13 20:46:17 -0500

    Add solution for largest component js

--- module5/largestComponent.js
+++ module5/largestComponent.js
@@ -44,3 +44,27 @@ APPROACH
 - we need to iterate through the nodes of the adjacency list
 - we can also use a DFS to traverse through the node components (and count)
 */
+
+const largestComponent = (graph) => {
+  let visited = new Set();
+  let longest = 0;
+
+  for (let node in graph) {
+    const size = exploreSize(graph, node, visited);
+    if (size > longest) longest = size;
+  }
+
+  return longest;
+};
+
+const exploreSize = (graph, node, visited) => {
+  if (visited.has(node)) return 0;
+  visited.add(node);
+
+  let size = 1; // this represents the CURRENT NODE as a size of 1
+  for (let neighbor of graph[node]) {
+    size += exploreSize(graph, neighbor, visited);
+  }
+
+  return size;
+};
