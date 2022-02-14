a4fb59f68ea318df093adb965e8d3e6adb5649a2
Woodstock <victorhe97@gmail.com>
2022-02-13 20:09:52 -0500

    Init largest component

---
+++ module5/largestComponent.js
@@ -0,0 +1,40 @@
+/*
+largest component
+
+Write a function, largestComponent, that takes in the adjacency list of an undirected graph. The function should return the size of the largest connected component in the graph.
+test_00:
+
+largestComponent({
+  0: ['8', '1', '5'],
+  1: ['0'],
+  5: ['0', '8'],
+  8: ['0', '5'],
+  2: ['3', '4'],
+  3: ['2', '4'],
+  4: ['3', '2']
+}); // -> 4
+
+test_01:
+
+largestComponent({
+  1: ['2'],
+  2: ['1','8'],
+  6: ['7'],
+  9: ['8'],
+  7: ['6', '8'],
+  8: ['9', '7', '2']
+}); // -> 6
+
+test_02:
+
+largestComponent({
+  3: [],
+  4: ['6'],
+  6: ['4', '5', '7', '8'],
+  8: ['6'],
+  7: ['6'],
+  5: ['6'],
+  1: ['2'],
+  2: ['1']
+}); // -> 5
+*/
