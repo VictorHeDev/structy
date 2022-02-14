b20ed3a5d1d759c15b4f8158b1b7d1ed69932110
Woodstock <victorhe97@gmail.com>
2022-02-14 13:55:06 -0500

    Init shortest path py

---
+++ module5/shortest_path.py
@@ -0,0 +1,42 @@
+'''
+shortest path
+
+Write a function, shortest_path, that takes in a list of edges for an undirected graph and two nodes (node_A, node_B). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1.
+test_00:
+
+edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+]
+
+shortest_path(edges, 'w', 'z') # -> 2
+
+test_01:
+
+edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+]
+
+shortest_path(edges, 'y', 'x') # -> 1
+
+test_02:
+
+edges = [
+  ['a', 'c'],
+  ['a', 'b'],
+  ['c', 'b'],
+  ['c', 'd'],
+  ['b', 'd'],
+  ['e', 'd'],
+  ['g', 'f']
+]
+
+shortest_path(edges, 'a', 'e') # -> 3
+'''
