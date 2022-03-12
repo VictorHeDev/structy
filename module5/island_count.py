78b7bf70c80acf8f9bbe5e119d0a43b06fa33228
Woodstock <victorhe97@gmail.com>
2022-03-12 15:44:02 -0500

    Add new island count

--- module5/island_count.py
+++ module5/island_count.py
@@ -25,3 +25,39 @@ def island_count(grid):
         counter += 1
 
   return counter
+
+
+def island_count(grid):
+  counter = 0
+  coordinates = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+  visited = set()
+
+  for row in range(len(grid)):
+    for col in range(len(grid[0])):
+      if dfs(grid, row, col, visited, coordinates) == True:
+        counter += 1
+
+  return counter
+
+def dfs(grid, row, col, visited, coordinates):
+  if not in_bounds(row, col, grid):
+    return False
+
+  if grid[row][col] == 'W':
+    return False
+
+  pos = (row, col)
+  if pos in visited:
+    return False
+  visited.add(pos)
+
+  for dx, dy in coordinates:
+    new_x, new_y = dx + row, dy + col
+    dfs(grid, new_x, new_y, visited, coordinates)
+  return True
+
+def in_bounds(row, col, grid):
+  row_inbounds = 0 <= row < len(grid)
+  col_inbounds = 0 <= col < len(grid[0])
+  return row_inbounds and col_inbounds
+
