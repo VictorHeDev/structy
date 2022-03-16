b9ac0dd5266aa52293e065fe3575b566a1449acf
Woodstock <victorhe97@gmail.com>
2022-03-16 17:57:36 -0400

    Solution for has cycle py

--- module5/has_cycle.py
+++ module5/has_cycle.py
@@ -1,32 +1,29 @@
 def has_cycle(graph):
-
+  '''
+  white-grey-black graph algorithm
+  used to detect cycles within a directed graph
+  white: unexplored
+  grey: visiting
+  black: visited
+  '''
   visiting = set()
   visited = set()
-
-  def traverse(node, visiting, visited):
-    if node in visited:
-      return False
-
-    if node in visiting:
+  for start_node in graph:
+    if cycle_detect(graph, start_node, visiting, visited):
       return True
-
-    visiting.add(node)
-
-    for neib in graph[node]:
-      if traverse(neib, visiting, visited):
-        return True
-
-    visiting.remove(node)
-    visited.add(node)
-
-    return False
-
-  for node in graph.keys():
-    if traverse(node, visiting, visited):
-      return True
-
   return False
 
+def cycle_detect(graph, node, visiting, visited):
+  if node in visited:
+    return False
+  if node in visiting:
+    return True
+  visiting.add(node)
 
-
-
+  for neighbor in graph[node]:
+    if cycle_detect(graph, neighbor, visiting, visited) == True:
+      return True
+  # change color from grey to black
+  visiting.remove(node)
+  visited.add(node)
+  return False
