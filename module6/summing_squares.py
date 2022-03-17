bd10d664648311523f65815ca42e238b3bdc9020
Woodstock <victorhe97@gmail.com>
2022-03-17 18:19:35 -0400

    memoized summing squares

--- module6/summing_squares.py
+++ module6/summing_squares.py
@@ -27,3 +27,21 @@ def summing_squares(n):
 
   return min_squares
 
+# memoized
+def summing_squares(n):
+  return _summing_squares(n, {})
+
+def _summing_squares(n, memo):
+  if n in memo:
+    return memo[n]
+  if n == 0:
+    return 0
+
+  min_squares = float('inf')
+  for i in range(1, math.floor(math.sqrt(n)) + 1):
+    square = i ** 2
+    num_squares = 1 + _summing_squares(n - square, memo)
+    min_squares = min(min_squares, num_squares)
+
+  memo[n] = min_squares
+  return memo[n]
