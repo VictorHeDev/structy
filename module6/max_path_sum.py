31a366f08fb42be8491deaf2709cc2aee6facdfe
Woodstock <victorhe97@gmail.com>
2022-03-17 16:59:01 -0400

    Memoized max path sum

--- module6/max_path_sum.py
+++ module6/max_path_sum.py
@@ -9,3 +9,23 @@ def max_path_sum(grid):
     for j in range(1, col_len):
       grid[i][j] += max(grid[i-1][j], grid[i][j-1])
   return grid[row_len-1][col_len-1]
+
+# memoized
+def max_path_sum(grid):
+  return _max_path_sum(grid, 0, 0, {})
+
+def _max_path_sum(grid, r, c, memo):
+  pos = (r, c)
+  if pos in memo:
+    return memo[pos]
+  if r == len(grid) or c == len(grid[0]):
+    # return an "invalid" number
+    return float('-inf')
+
+  if r == len(grid) - 1 and c == len(grid[0]) - 1:
+    return grid[r][c]
+
+  down = _max_path_sum(grid, r + 1, c, memo)
+  right = _max_path_sum(grid, r, c + 1, memo)
+  memo[pos] = grid[r][c] + max(down, right)
+  return memo[pos]
