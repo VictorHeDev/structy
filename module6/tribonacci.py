ac85ae95e28f833ae4ce9f44c5be46151ac3cfae
Woodstock <victorhe97@gmail.com>
2022-03-16 21:34:16 -0400

    Sol for tribonacci py

--- module6/tribonacci.py
+++ module6/tribonacci.py
@@ -1,12 +1,14 @@
-def tribonacci(n, memo=None):
-  if n <= 0:
+def tribonacci(n):
+  memo = {}
+  return _tribonacci(n, memo)
+
+def _tribonacci(n, memo):
+  if n in memo:
+    return memo[n]
+  if n == 0 or n == 1:
     return 0
   if n == 2:
     return 1
-  if memo is None:
-    memo = {}
-  if n in memo:
-    return memo[n]
-  memo[n] = tribonacci(n-1, memo) + tribonacci(n-2, memo) + tribonacci(n-3, memo)
-  return memo[n]
 
+  memo[n] = _tribonacci(n - 1, memo) + _tribonacci(n - 2, memo) + _tribonacci(n - 3, memo)
+  return memo[n]
